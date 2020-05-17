cfd253f6b7020a8fbddc385920056398b07913f9
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2020-05-17 15:51:59 +0200

    Mangler DDOS delen...

---
+++ client.py
@@ -0,0 +1,177 @@
+import socket
+from datetime import datetime
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor
+import re
+
+# CLIENT CONSTANTS
+SERVER = socket.gethostbyname(socket.gethostname())
+PORT = 1337
+ADDR = (SERVER, PORT)
+HEADER = 8
+FORMAT = 'utf-8'
+DDOS_MESSAGE = "can you handle this?"
+LAST_MESSAGE_TIME = datetime.now()
+MSG_COUNT = 0
+# A means to be able to stop while loops in other threads
+ACTIVE = True
+
+# Default these to false, then check config for true
+KEEP_ALIVE = False
+DDOS_ACTIVE = False
+# READ CONFIG FOR CONSTANT VALUES
+config = open("opt.config", "r")
+
+lines = config.readlines()
+for line in lines:
+    if line.startswith("KEEP_ALIVE : True"):
+        KEEP_ALIVE = True
+    if line.startswith("DDOS_ACTIVE : True"):
+        DDOS_ACTIVE = True
+    if line.startswith("DDOS_AMOUNT"):
+        DDOS_AMOUNT = int(line.replace("DDOS_AMOUNT : ", ""))
+
+config.close()
+
+# SOCKET VARIABLE
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def send(msg):
+    # Define a new variable and encode it to bytes with given FORMAT constant
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    LAST_MESSAGE_TIME = datetime.now()
+
+
+def receive_msg():
+    # Define a variable from the received header, which must be equal (in bytes) to the HEADER constant
+    # Decode the received bytestream with the defined FORMAT constant
+    msg_length = client.recv(HEADER).decode(FORMAT)
+    # Checks not null (if msg_length is not null)
+    if msg_length:
+        # Set the msg_length variable equal to int typecast of the received (and decoded) bytes.
+        msg_length = int(msg_length)
+        return client.recv(msg_length).decode(FORMAT)
+
+
+def listen():
+    global ACTIVE, MSG_COUNT
+    while ACTIVE:
+        try:
+            msg = receive_msg()
+            if msg == "con-res 0xFE":
+                # Protocol response to disconnect
+                send("con-res 0xFF")
+                client.close()
+                ACTIVE = False
+                raise ConnectionResetError
+
+            # If the received message count minus the last sent message is not 1, then raise exception
+            # I use re.search to look between the two substrings "res-" and "=" for the count
+            received_msg_count = int(re.search('res-(.*)=', msg).group(1))
+            if received_msg_count - MSG_COUNT != 1 and received_msg_count != 0:
+                raise ConnectionResetError
+
+            # Set the new message count to the received plus 1
+            MSG_COUNT = received_msg_count + 1
+
+        except ConnectionResetError:
+            break
+
+        print(msg)
+        # Simply for performance
+        time.sleep(0.1)
+
+
+def talk():
+    global LAST_MESSAGE_TIME, ACTIVE, MSG_COUNT
+    while ACTIVE:
+        print("[INPUT]:")
+        try:
+            msg = input()
+            send(f"msg-{MSG_COUNT}={msg}")
+            LAST_MESSAGE_TIME = datetime.now()
+        except OSError:
+            print("Connection no longer active")
+
+
+def heartbeat():
+    global LAST_MESSAGE_TIME
+    while KEEP_ALIVE:
+        # Checks if time since last message is more than 3 seconds
+        time_since_last_message = datetime.now() - LAST_MESSAGE_TIME
+        if time_since_last_message.total_seconds() > 3:
+            try:
+                send("con-h 0x00")
+                LAST_MESSAGE_TIME = datetime.now()
+            except ConnectionResetError:
+                print("Lost connection to server")
+                break
+        # Simply for performance
+        time.sleep(0.1)
+
+
+def DDOS():
+    while DDOS_ACTIVE:
+        send(DDOS_MESSAGE)
+        time.sleep(1 / DDOS_AMOUNT)
+
+
+# Start of Client
+def connect_to_server():
+    # Client tries to connect to server
+    # If connection is refused, it tries again 1 second later
+    while True:
+        print(f"Attempting to connect to {SERVER} on port {PORT}")
+        try:
+            client.connect(ADDR)
+            print("Connected to server socket")
+
+            # NORMAL CONNECTION
+            init_handshake()
+
+            break
+        except ConnectionRefusedError:
+            print(f"Connection was refused, server may be down or on a different address")
+            print("Trying again in 1 sec...")
+            time.sleep(1)
+
+
+# Asks server if the client ip can join
+def init_handshake():
+    # Handshake step 1
+    send("com-0 " + socket.gethostbyname(socket.gethostname()))
+    print(f"Sending request to join from [{socket.gethostbyname(socket.gethostname())}]")
+    reply_msg = receive_msg()
+    print(f"[SERVER] {reply_msg}")
+    if reply_msg.startswith("com-0 accept ") and ip_validator(reply_msg.replace("com-0 accept ", "")):
+        # Handshake step 3
+        send("com-0 accept")
+        listen_thread = threading.Thread(target=listen).start()
+        heartbeat_thread = threading.Thread(target=heartbeat).start()
+        ddos_thread = threading.Thread(target=DDOS).start()
+        talk()
+
+    else:
+        # Handshake step 3
+        send("com-0 deny")
+        print("Connection denied")
+        client.close()
+
+
+def ip_validator(string):
+    try:
+        # Check for valid IP address
+        socket.inet_aton(string)
+        return True
+    except socket.error:
+        return False
+
+
+connect_to_server()
---
+++ server.py
@@ -0,0 +1,145 @@
+import socket
+import threading
+from datetime import datetime
+import time
+import re
+
+# SERVER CONSTANTS
+PORT = 1337
+# Henter min lokale IP
+SERVER_IP = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER_IP, PORT)
+# Antal bytes for headeren
+HEADER = 8
+FORMAT = 'utf-8'
+# SOCKET VARIABLE
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(ADDR)
+
+# READ CONFIG FOR CONSTANT VALUES
+config = open("opt.config", "r")
+
+lines = config.readlines()
+for line in lines:
+    if line.startswith("MAX_PACKETS"):
+        MAX_PACKETS = int(line.replace("MAX_PACKETS : ", ""))
+
+config.close()
+
+
+def log(conn, str):
+    # "a" for append
+    logger = open("handshakes.log", "a")
+    logger.write(f"[{conn}] {datetime.now()} {str}\n")
+    logger.close()
+
+
+def start_server():
+    # Listen on the server socket
+    server_socket.listen()
+    print(f"{datetime.now()} [LISTENING] Server is listening on {SERVER_IP}")
+    while True:
+        conn, addr = server_socket.accept()
+        # Receive first message from client
+        client_request_msg = receive_msg(conn)
+        log(addr, client_request_msg)
+
+        if ip_validator(client_request_msg.replace("com-0 ", "")):
+            # Send accept message if it's not / Handshake step 2
+            reply_message = f"com-0 accept {SERVER_IP}"
+            send(conn, reply_message)
+            # Log handshake step 2
+            log(ADDR, reply_message)
+            client_reply_message = receive_msg(conn)
+            # Log handshake step 3
+            log(addr, client_reply_message)
+            print(f"[CLIENT]: {client_reply_message}")
+            client_thread = threading.Thread(target=client_handler, args=(conn, addr))
+            client_thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        else:
+            # Send deny message if it is not correct IP / Handshake step 2
+            reply_message = f"com-0 deny {SERVER_IP}"
+            send(conn, reply_message)
+            # Log handshake step 1
+            log(ADDR, reply_message)
+            print(f"[CLIENT]: {receive_msg(conn)}")
+
+
+def ip_validator(string):
+    try:
+        # Check for valid IP address
+        socket.inet_aton(string)
+        return True
+    except socket.error:
+        return False
+
+
+def send(conn, msg):
+    # Define a new variable and encode it to bytes with given FORMAT constant
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    conn.send(send_length)
+    conn.send(message)
+    LAST_SENT_MESSAGE_TIME = datetime.now()
+
+
+def receive_msg(conn):
+    # Define a variable from the received header, which must be equal (in bytes) to the HEADER constant
+    # Decode the received bytestream with the defined FORMAT constant
+    msg_length = conn.recv(HEADER).decode(FORMAT)
+    # Checks not null (if msg_length is not null)
+    if msg_length:
+        # Set the msg_length variable equal to int typecast of the received (and decoded) bytes.
+        msg_length = int(msg_length)
+        return conn.recv(msg_length).decode(FORMAT)
+
+
+def client_handler(conn, addr):
+    msg_count = 0
+    print(f"{threading.currentThread()} started")
+    print(f"{datetime.now()} [NEW CONNECTION] {addr} connected.")
+
+    # Start main messaging loop
+    while True:
+        # Reset the socket timeout to 4 seconds, at the beginning of each loop.
+        # If no message is received within time, connection is timed out.
+        conn.settimeout(4)
+        try:
+            msg = receive_msg(conn)
+            # If the received message does not match the tolerance disconnect reply
+            # or the heartbeat message "con-h 0x00"
+            # then we print to console.
+            if msg != "con-res 0xFF" and msg != "con-h 0x00":
+                print(f"[{addr}] {msg}")
+                # If the received message count minus the last sent message is not 1, then raise exception
+                # I use re.search to look between the two substrings "msg-" and "=" for the count
+                received_msg_count = int(re.search('msg-(.*)=', msg).group(1))
+                if received_msg_count - msg_count != 1 and received_msg_count != 0:
+                    raise ConnectionRefusedError
+                # Set the new message count to the received plus 1
+                msg_count = received_msg_count + 1
+
+                send(conn, f"res-{msg_count}=I am server")
+
+        # This code is then run in the case of a timeout
+        except socket.timeout:
+            send(conn, "con-res 0xFE")
+            receive_msg(conn)
+            print(f"{datetime.now()} [{addr}] was disconnected due to inactivity")
+            break
+        # This code is run if the client terminates the connection
+        except ConnectionResetError:
+            print(f"{datetime.now()} [{addr}] terminated the connection")
+            break
+        except ConnectionRefusedError:
+            send(conn, "con-res 0xFE")
+            print(f'{datetime.now()} [{addr}] sent illegal message count')
+            break
+
+        time.sleep(1 / MAX_PACKETS)
+
+
+start_server()
